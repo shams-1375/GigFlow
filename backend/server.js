958195db92001a0a5682a29e9fd8547fc85845b6
Mohammed Shams Ahmed <ahmedshams1375@gmail.com>
2026-01-13 19:10:31 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
