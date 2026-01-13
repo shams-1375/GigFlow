e7fb3b45a3e09370b74056dbcf23bda3fad2252d
Mohammed Shams Ahmed <ahmedshams1375@gmail.com>
2026-01-13 18:05:10 +0530

    Added Backend URL

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gigflow-backend-e951.onrender.com",
   withCredentials: true
 });
 
