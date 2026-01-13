3ad95be728e28dded8577a7ee001d17d4bfac9cd
Mohammed Shams Ahmed <ahmedshams1375@gmail.com>
2026-01-13 19:27:31 +0530

    Fixed URL

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
 
+
+app.use(cors({
+  origin: "https://gigflow-frontend-iya8.onrender.com",
+  credentials: true,                                   
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gigflow-backend-e951.onrender.com",
+  baseURL: "https://gigflow-backend-e951.onrender.com/api",
   withCredentials: true
 });
 
